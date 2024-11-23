106f5afbbe85d7d6536b05339ca98a2aaad327bb
rohankalbag <kalbag.rohan.r@gmail.com>
2024-11-23 23:29:23 +0530

    added featuring of cover and group photos from portal

--- generateYearBook.py
+++ generateYearBook.py
@@ -35,7 +35,7 @@ def add_message_block(c, message, y_position):
     if profile_image_url:
         response = requests.get("https://centralised.sarc-iitb.org" + profile_image_url, headers=auth_header)
         profile_img = ImageReader(BytesIO(response.content))
-        c.drawImage(profile_img, width - 110, y_position - 60, 50, 50)
+        c.drawImage(profile_img, width - 260, y_position - 60, 50, 50)
 
     # Names and details
     written_by = "From: " + message['written_by']
@@ -43,23 +43,49 @@ def add_message_block(c, message, y_position):
     c.setFont("Helvetica-Bold", 12)
     c.setFillColor(colors.purple)
     c.drawString(120, y_position - 20, f"{written_by}")
-    c.drawString(width - 280, y_position - 20, f"{written_for}")
+    c.drawString(width - 200, y_position - 20, f"{written_for}")
 
     c.setFont("Helvetica", 10)
     c.setFillColor(colors.black)
     if message['written_by_dept'] != "None" and message['written_by_year'] != "None":
         # for anonymous users, these fields are not present
-        c.drawString(120, y_position - 40, f"{message['written_by_dept']} | {message['written_by_year']}")
-    c.drawString(width - 280, y_position - 40, f"{message['written_for_dept']} | {message['written_for_year']}")
+        if '&' in message["written_by_dept"]:
+            p = message["written_by_dept"].split('&')
+            c.drawString(120, y_position - 40, f"{p[0]}")
+            c.drawString(120, y_position - 55, f"{'&' + p[1]}")
+            c.drawString(120, y_position - 70, f"{message['written_by_year']}")
+        elif 'and' in message["written_by_dept"]:
+            p = message["written_by_dept"].split('and')
+            c.drawString(120, y_position - 40, f"{p[0]}")
+            c.drawString(120, y_position - 55, f"{'and' + p[1]}")
+            c.drawString(120, y_position - 70, f"{message['written_by_year']}")
+        else:
+            c.drawString(120, y_position - 40, f"{message['written_by_dept']}")
+            c.drawString(120, y_position - 55, f"{message['written_by_year']}")
+    
+    if '&' in message["written_for_dept"]:
+        p = message["written_for_dept"].split('&')
+        c.drawString(width - 200, y_position - 40, f"{p[0]}")
+        c.drawString(width - 200, y_position - 55, f"{'&' + p[1]}")
+        c.drawString(width - 200, y_position - 70, f"{message['written_for_year']}")
+    elif ' and ' in message["written_for_dept"]:
+        p = message["written_for_dept"].split('and')
+        c.drawString(width - 200, y_position - 40, f"{p[0]}")
+        c.drawString(width - 200, y_position - 55, f"{'and' + p[1]}")
+        c.drawString(width - 200, y_position - 70, f"{message['written_for_year']}")
+    else:
+        c.drawString(width - 200, y_position - 40, f"{message['written_for_dept']}")
+        c.drawString(width - 200, y_position - 55, f"{message['written_for_year']}")
+
 
     # Written Content
     c.setFont("Symbola",12)
     content = message['content']
-    text_object = c.beginText(60, y_position - 80)
+    text_object = c.beginText(60, y_position - 85)
     text_object.setFont("Symbola", 12)
     max_width = width - 120
     content_height = 0 
-    current_y_position = y_position - 80
+    current_y_position = y_position - 85
 
     for line in content.splitlines():
         words = line.split()
@@ -99,20 +125,49 @@ def add_message_block(c, message, y_position):
 
     return current_y_position - 50
 
-def generate_pdf(messagesForYou, messagesByYou, output_file):
+def generate_pdf(messagesForYou, messagesByYou, userPhotos, output_file):
     c = canvas.Canvas(output_file, pagesize=letter)
+    width, height = letter
 
     c.setFont("Helvetica-Bold", 20)
     c.setFillColor(colors.purple)
-    c.drawString(50, 750, "Yearbook - Your College Memories")
+    c.drawString(125, 750, "Yearbook - Your College Memories")
     c.setFont("Helvetica", 12)
-    c.drawString(50, 730, "Cherish the messages written for you and by you.")
+    c.drawString(100, 730, "Let's take a stroll through time and remember the good times we had together!")
 
-    c.setFont("Helvetica-Bold", 15)
-    c.setFillColor(colors.purple)
-    c.drawString(50, 700, "What people wrote for you")
+    c.setStrokeColor(colors.purple)
+    c.setLineWidth(1)
+    c.line(50, 720, width - 50, 720)
 
-    y_position = 680
+    imagesInserted = False
+
+    try:
+        coverImageURL = "https://centralised.sarc-iitb.org" + userPhotos["cover"]
+        response = requests.get(coverImageURL, headers=auth_header)
+        coverImage = ImageReader(BytesIO(response.content))
+        c.drawImage(coverImage, 50, 545, width - 100, (width - 100)*0.3) # 10:3 aspect ratio
+        groupImageURLs = [userPhotos[t] for t in userPhotos if t.startswith("img")]
+        groupImages = [ImageReader(BytesIO(requests.get("https://centralised.sarc-iitb.org" + groupImageURL, headers=auth_header).content)) for groupImageURL in groupImageURLs]
+        c.setFillColor(colors.purple)
+        c.setFont("Helvetica", 12)
+        c.drawString(175, 510, "Refresh your memories with these group photos!")
+        for i in range(len(groupImages)):
+            c.drawImage(groupImages[i], 50 + (i % 2) * 265, 310 - (i // 2) * 200, (width - 110)/2, (width - 110)/2 * (2/3)) # 3:2 aspect ratio
+        imagesInserted = True
+    except:
+        time.sleep(0)
+
+    if imagesInserted:
+        c.showPage()
+        c.setFont("Helvetica-Bold", 20)
+        c.setFillColor(colors.purple)
+        c.drawString(175, 750, "What people wrote for you")
+        y_position = 720
+    else:
+        c.setFont("Helvetica-Bold", 20)
+        c.setFillColor(colors.purple)
+        c.drawString(175, 710, "What people wrote for you")
+        y_position = 690
     progressBar = tqdm(total=len(messagesForYou) + len(messagesByYou))
 
     for message in messagesForYou:
@@ -122,11 +177,13 @@ def generate_pdf(messagesForYou, messagesByYou, output_file):
         y_position = add_message_block(c, message, y_position)
         progressBar.update(1)
     
-    c.setFont("Helvetica-Bold", 15)
+    
+    c.showPage()
+    c.setFont("Helvetica-Bold", 20)
     c.setFillColor(colors.purple)
-    c.drawString(50, y_position, "What you wrote for others")
+    c.drawString(175, 750, "What you wrote for others")
 
-    y_position -= 30
+    y_position = 720
     
     for message in messagesByYou:
         if y_position < 150: 
@@ -154,4 +211,5 @@ if __name__ == "__main__":
     userId = requests.get("https://centralised.sarc-iitb.org/api/authenticate/current_user/", headers=auth_header).json()['id']
     messagesForYou = requests.get(f"https://centralised.sarc-iitb.org/api/posts/others/{userId}",headers=auth_header).json()
     messagesByYou = requests.get(f"https://centralised.sarc-iitb.org/api/posts/my/{userId}",headers=auth_header).json()
-    generate_pdf(messagesForYou, messagesByYou, "yearbook.pdf")
+    userPhotos = requests.get(f"https://centralised.sarc-iitb.org/api/authenticate/profile/{userId}/gallery/", headers=auth_header).json()
+    generate_pdf(messagesForYou, messagesByYou, userPhotos, "yearbook.pdf")
